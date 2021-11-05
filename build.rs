cc86dac259c6f5aeb27f537417fbc0b15eb5ce14
hezuikn <hezuikn>
2021-11-05 13:46:41 +0000

    minimized

--- build.rs
+++ build.rs
@@ -1,6 +1,3 @@
 fn main()
 {
-    let path = std::env::var("CARGO_BIN_FILE_TEST").unwrap();
-    //make the artifacts available to the test at runtime
-    println!("cargo:rustc-env={}={}", "CRATE_BIN", path);
 }
--- test/src/main.rs
+++
@@ -1,4 +0,0 @@
-fn main()
-{
-	println!("if you read this it werks");
-}
