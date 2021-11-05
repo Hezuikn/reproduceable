d2182610e9c1c1dbf46fe7e99decbec26e05a81c
Hezuikn <68823892+Hezuikn@users.noreply.github.com>
2021-11-05 07:25:10 +0000

    comment worthy

--- build.rs
+++ build.rs
@@ -1,5 +1,6 @@
 fn main()
 {
     let path = std::env::var("CARGO_BIN_FILE_TEST").unwrap();
+    //make the artifacts available to the test at runtime
     println!("cargo:rustc-env={}={}", "CRATE_BIN", path);
 }
